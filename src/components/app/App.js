15f6e4062267f402875b2973b9ef81f9a7f44f01
Eugene <example@gmail.com>
2022-08-26 13:42:43 +0300

    :charlist

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react/cjs/react.development";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -5,20 +6,26 @@ import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+    state = {
+        showRandomChar: true
+    }
+
+    render () {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    {this.state.showRandomChar ? <RandomChar/> : null}
+                    <div className="char__content">
+                        <CharList/>
+                        <CharInfo/>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
