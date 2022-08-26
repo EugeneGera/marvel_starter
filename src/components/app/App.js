cee6b3bed7cd325c123c4979fd7a70fded41aa8d
Eugene <example@gmail.com>
2022-08-26 15:09:58 +0300

    :charinfo mod

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react/cjs/react.development";
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,19 +7,26 @@ import CharInfo from "../charInfo/CharInfo";
 import decoration from '../../resources/img/vision.png';
 
 class App extends Component {
+    
     state = {
-        showRandomChar: true
+        selectedChar: null
     }
 
-    render () {
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    render() {
         return (
             <div className="app">
                 <AppHeader/>
                 <main>
-                    {this.state.showRandomChar ? <RandomChar/> : null}
+                    <RandomChar/>
                     <div className="char__content">
-                        <CharList/>
-                        <CharInfo/>
+                        <CharList onCharSelected={this.onCharSelected}/>
+                        <CharInfo charId={this.state.selectedChar}/>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
